0e916030d3cbe78b6b718dc40812f3ac62734096
蒋琦 <jiangqi@51ucar.cn>
2019-01-10 17:38:22 +0800

    t

--- resources/views/web/list.blade.php
+++ resources/views/web/list.blade.php
@@ -657,28 +657,7 @@
         };
     </script>
     <script src="./js/CategoryList.js" type="text/javascript"></script>
-    <div class="footer">
-
-        <div class="footer_nav">
-            <a href="http://center.uuhooo.com/PageSite/Home?WeiXinID=oF6TCjgFlTmhIKw9BfvectCBN_Js&amp;ShopWeiXinID=gh_88b3f2b378ad" data-subhref="/PageSite/Home">
-                <i class="icon_footer_home">
-                </i>首页</a>
-            <a href="http://center.uuhooo.com/Item/CategoryList?WeiXinID=oF6TCjgFlTmhIKw9BfvectCBN_Js&amp;ShopWeiXinID=gh_88b3f2b378ad" data-subhref="/Item/CategoryList" class="active">
-                <i class="icon_footer_cat">
-                </i>商品分类</a>
-            <a href="http://m.uuhooo.com/integralmall/integralexchange?shopweixinid=gh_88b3f2b378ad" data-subhref="http://m.uuhooo.com/integralmall/integralexchange?shopweixinid=gh_88b3f2b378ad">
-                <i class="icon_footer_brand">
-                </i>积分商城</a>
-            <a href="http://member.uuhooo.com/Member/MemberSign?shopweixinid=gh_88b3f2b378ad" data-subhref="http://member.uuhooo.com/Member/MemberSign?shopweixinid=gh_88b3f2b378ad">
-                <i class="icon_footer_orders">
-                </i>签到赠送</a>
-            <a href="http://center.uuhooo.com/Setting/Index?WeiXinID=oF6TCjgFlTmhIKw9BfvectCBN_Js&amp;ShopWeiXinID=gh_88b3f2b378ad" data-subhref="/Setting/Index">
-                <i class="icon_footer_personal">
-                    <em></em>
-                </i>个人中心</a>
-        </div>
-
-    </div>
+    @include('web.layouts.footer')
     <script>
         $(function () {
             RefreshShoppingCar();
